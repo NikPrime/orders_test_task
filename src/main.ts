3a4e839eff5bc96770f4f932b9eca6c8d08833c5
Nikita Antipin <nikprime1@gmail.com>
2022-10-11 12:34:09 +0600

    added swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -11,6 +12,11 @@ async function bootstrap() {
             whitelist: true,
         }),
     );
+
+    const swaggerDocBuilder = new DocumentBuilder().setTitle('Orderbook docs').setDescription('test task for sfxdx').build();
+
+    const document = SwaggerModule.createDocument(app, swaggerDocBuilder);
+    SwaggerModule.setup('api', app, document);
 
     await app.listen(3000);
 }
